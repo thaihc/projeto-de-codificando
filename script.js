7b323e618c2fee434bd40cc42ba424cf850eacbe
thaihc <thaisherculanodecarvalho@gmail.com>
2021-12-09 16:21:29 -0300

    atualizacao-final-js

--- script.js
+++ script.js
@@ -28,24 +28,38 @@ decode.addEventListener("click", function () {
 
 // 4- Atividade que liga os Radio buttons para codificar ou decodificar
 botao.addEventListener("click", function (event) {
+  event.preventDefault();
   if (code.checked && selecao.value == "cesar") {
     mensagemFinal.value = codeCesar(mensagem.value, parseInt(incremento.value));
   } else if (decode.checked && selecao.value == "cesar") {
     mensagemFinal.value = decodeCesar(
-      mensagem.value,
+      mensagem.value.split(""),
       parseInt(incremento.value)
     );
   } else if (code.checked && selecao.value == "bs64") {
-    mensagemFinal.innerText = codeBase64(mensagem.value);
+    mensagemFinal.value = codeBase64(mensagem.value);
   } else if (decode.checked && selecao.value == "bs64") {
-    mensagemFinal.innerText = decodeBase64(mensagem.value);
+    mensagemFinal.value = decodeBase64(mensagem.value);
   }
-  event.preventDefault();
 });
 
 // 5- Função para codificar em Cifra de César
 function codeCesar(msg, passo) {
-  var resultC = "";
+  var resultD = [];
+  var decodeC;
+  for (var i = 0; i < msg.length; i++) {
+    if (msg[i].charCodeAt() >= 65 && msg[i].charCodeAt() <= 90) {
+      decodeC = ((msg[i].charCodeAt() - 65 + passo) % 26) + 65;
+      resultD.push(String.fromCharCode(decodeC));
+    } else if (msg[i].charCodeAt() >= 97 && msg[i].charCodeAt() <= 122) {
+      decodeC = ((msg[i].charCodeAt() - 97 + passo) % 26) + 97;
+      resultD.push(String.fromCharCode(decodeC));
+    } else {
+      resultD.push(msg[i]);
+    }
+  }
+  return resultD.join("");
+  /*var resultC = ;
   var codeC = 0;
   for (var i = 0; i < msg.length; i++) {
     if (msg.charCodeAt(i) >= 65 && msg.charCodeAt(i) <= 90) {
@@ -57,32 +71,33 @@ function codeCesar(msg, passo) {
     }
     resultC += String.fromCharCode(codeC);
   }
-  return resultC.toLowerCase();
+  return resultC.toLowerCase();*/
 }
 
 // 5- Função para decodificar em Cifra de César
 function decodeCesar(msg, passo) {
-  var resultD = "";
-  var decodeC = 0;
+  var resultD = [];
+  var decodeC;
   for (var i = 0; i < msg.length; i++) {
-    if (msg.charCodeAt(i) >= 65 && msg.charCodeAt(i) <= 90) {
-      if (msg.charCodeAt(i) - 65 - passo < 0) {
-        decodeC = ((msg.charCodeAt(i) - 65 - passo + 26) % 26) + 65;
-      } else {
-        decodeC = ((msg.charCodeAt(i) - 65 - passo) % 26) + 65;
-      }
-    } else if (msg.charCodeAt(i) >= 97 && msg.charCodeAt(i) <= 122) {
-      if (msg.charCodeAt(i) - 97 - passo < 0) {
-        decodeC = ((msg.charCodeAt(i) - 97 - passo + 26) % 26) + 97;
-      } else {
-        decodeC = ((msg.charCodeAt(i) - 97 - passo) % 26) + 97;
-      }
-    } else if (msg.charCodeAt(i) === 32) {
-      decodeC = 32;
+    if (msg[i].charCodeAt() >= 65 && msg[i].charCodeAt() <= 90) {
+      //if (msg.charCodeAt(i) - 65 - passo < 0) {
+      decodeC = ((msg[i].charCodeAt() - 90 - passo) % 26) + 90;
+      resultD.push(String.fromCharCode(decodeC));
+      //} else {
+      //decodeC = ((msg.charCodeAt(i) - 65 - passo) % 26) + 65;
+      //}
+    } else if (msg[i].charCodeAt() >= 97 && msg[i].charCodeAt() <= 122) {
+      //if (msg.charCodeAt(i) - 97 - passo < 0) {
+      decodeC = ((msg[i].charCodeAt() - 122 - passo) % 26) + 122;
+      resultD.push(String.fromCharCode(decodeC));
+      //} else {
+      // decodeC = ((msg.charCodeAt(i) - 97 - passo) % 26) + 97;
+      //}
+    } else {
+      resultD.push(msg[i]);
     }
-    resultD += String.fromCharCode(decodeC);
   }
-  return resultD.toLowerCase();
+  return resultD.join("");
 }
 
 // 4- Função para codificar em Base64
